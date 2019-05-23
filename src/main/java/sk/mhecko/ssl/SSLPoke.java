b3f9b7bc545bbe7da1d75b8ac0d2750cde4f6420
Mauro Malvestio <mauro@malvestio.net>
2019-05-23 14:06:24 +0200

    improved version with certificate dump

--- src/main/java/sk/mhecko/ssl/SSLPoke.java
+++ src/main/java/sk/mhecko/ssl/SSLPoke.java
@@ -1,9 +1,15 @@
 package sk.mhecko.ssl;
 
+import java.security.cert.CertPath;
+import java.security.cert.CertificateFactory;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 import java.io.InputStream;
 import java.io.OutputStream;
+import javax.net.ssl.SSLSession;
 
 /**
  * Created by michalh on 21. 4. 2017.
@@ -20,8 +26,19 @@ public class SSLPoke {
             SSLSocketFactory sslsocketfactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
             SSLSocket sslsocket = (SSLSocket) sslsocketfactory.createSocket(args[0], Integer.parseInt(args[1]));
 
+            SSLSession session = sslsocket.getSession();
+            java.security.cert.Certificate[] servercerts = session.getPeerCertificates();
+
+            List mylist = new ArrayList();
+            for (int i = 0; i < servercerts.length; i++) {
+                mylist.add(servercerts[i]);
+            }
+
             InputStream in = sslsocket.getInputStream();
             OutputStream out = sslsocket.getOutputStream();
+
+            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+            CertPath cp = cf.generateCertPath(mylist);
 
             // Write a test byte to get a reaction :)
             out.write(1);
@@ -29,6 +46,7 @@ public class SSLPoke {
             while (in.available() > 0) {
                 System.out.print(in.read());
             }
+            System.out.println(cp);
             System.out.println("Successfully connected");
 
         } catch (Exception exception) {
